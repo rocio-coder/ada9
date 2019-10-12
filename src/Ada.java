085601afd58fe60210019bfaca0217295d9475ba
rocio-corder <suarez.rocio96@gmail.com>
2019-10-11 21:22:44 -0300

    Initial commit

---
+++ src/Ada.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Ada {
+//    public static void main(String[] args) {
+//        int calificacionuno, calificaciondos, calificaciontres, calificacioncuatro,sumaTotal,promediofinal ;
+//        Scanner scanner = new Scanner (System.in);
+//        System.out.println("ingrese calificacionuno: ");
+//        calificacionuno  = scanner.int();
+//        System.out.println("ingrese calificacion uno: ");
+//        calificaciondos = scanner.int();
+//        System.out.println("ingrese calificacion uno: ");
+//        calificaciontres = scanner.int();
+//        System.out.println("ingrese calificacion uno: ");
+//        calificacioncuatro = scanner.int();
+//        System.out.println("ingrese calificacion uno: ");
+//
+//        sumaTotal = (calificacionuno+calificaciondos+calificaciontres+calificacioncuatro);
+//     promediofinal= (sumaTotal/4);
+//    System.out.println("el promedio final es:"+""+promediofinal);
+//    }
+}
